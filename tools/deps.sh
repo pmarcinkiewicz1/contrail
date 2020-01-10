57ec42d5227b8d67021854f68bfff57fdad31e3d
pmarcinkiewicz1 <39917193+pmarcinkiewicz1@users.noreply.github.com>
2020-01-10 12:57:13 +0100

    Update deps.sh

--- tools/deps.sh
+++ tools/deps.sh
@@ -10,7 +10,7 @@ go install ./vendor/github.com/mitchellh/gox
 go install ./vendor/github.com/pseudomuto/protoc-gen-doc/cmd/protoc-gen-doc
 go install ./vendor/golang.org/x/tools/cmd/goimports
 
-curl -sfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | \
+curl -sfL --insecure https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | \
 	bash -s -- -b $(go env GOPATH)/bin v1.10.2
 
 if [[ $(./bin/protoc --version) == "libprotoc 3.5.1" ]]; then
@@ -19,11 +19,11 @@ if [[ $(./bin/protoc --version) == "libprotoc 3.5.1" ]]; then
 fi
 
 if [ "$(uname)" == 'Darwin' ]; then
-    wget https://github.com/google/protobuf/releases/download/v3.5.1/protoc-3.5.1-osx-x86_64.zip
+    wget --no-check-certificate https://github.com/google/protobuf/releases/download/v3.5.1/protoc-3.5.1-osx-x86_64.zip
     unzip -o protoc-3.5.1-osx-x86_64.zip "bin/protoc"
     rm protoc-3.5.1-osx-x86_64.zip
 elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
-    wget https://github.com/google/protobuf/releases/download/v3.5.1/protoc-3.5.1-linux-x86_64.zip
+    wget --no-check-certificate https://github.com/google/protobuf/releases/download/v3.5.1/protoc-3.5.1-linux-x86_64.zip
     unzip -o protoc-3.5.1-linux-x86_64.zip "bin/protoc"
     rm protoc-3.5.1-linux-x86_64.zip
 else
